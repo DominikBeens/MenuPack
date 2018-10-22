949d0781114a2c78a63ed45c248731c0651ebcb8
Dominik Beens <dominik.beens@hotmail.nl>
2018-10-22 19:36:48 +0200

    initial commit

---
+++ MenuPack/Assets/Testing/DebugSceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using DB.MenuPack;
+
+public class DebugSceneLoader : MonoBehaviour 
+{
+
+    [SerializeField] private int sceneIndexToLoad;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.instance.LoadScene(sceneIndexToLoad, false);
+        }
+    }
+}
